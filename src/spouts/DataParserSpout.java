4b08df541dd23335cbce6c97a8087ccb5214620e
michlub665 <mlubecki6@wp.pl>
2019-08-23 15:05:36 +0200

    Sprawdzam wykorzystanie parsowania inputa

--- src/spouts/DataParserSpout.java
+++ src/spouts/DataParserSpout.java
@@ -1,4 +1,6 @@
 package spouts;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -8,25 +10,47 @@ import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import model.PSRCATEntry;
+import model.PSRCATParser;
 
-public class RandomNumberSpout implements IRichSpout {
+public class DataParserSpout implements IRichSpout {
 	private SpoutOutputCollector collector;
 	private TopologyContext context;
-	Random rng;
+	String dataFilePath = "random path";
+	PSRCATParser parser = new PSRCATParser();
+	List<PSRCATEntry> candidateList;
+	
 	@Override
 	public void open(Map conf, TopologyContext context,
 			SpoutOutputCollector collector) {
 		this.context = context;
 		this.collector = collector;
-		rng = new Random();
+		//Code of logic
+		if(parser.is_catalogue_file(dataFilePath)) {
+			if(parser.file_exists(dataFilePath)) {
+				candidateList = parser.parse_as_list(dataFilePath);
+			}
+		}
+		
+		
+		
+		
+		//
+		
 	}
 
 	@Override 
 	public void nextTuple() {
-		for(int i=0; i<10; i++) {
-			Float num = rng.nextFloat()*10;
-			collector.emit(new Values(num), num);
+		//collector.emit
+		
+		for(Iterator<PSRCATEntry> i = candidateList.iterator(); i.hasNext();) {
+			collector.emit(i.next());
 		}
+		
+//		for(int i=0; i<10; i++) {
+//			Float num = rng.nextFloat()*10;
+//			collector.emit(new Values(num), num);
+//		}
 	}
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
